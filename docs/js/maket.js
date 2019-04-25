e031046474796e23b5b31ff73a8403c80a77b19e
Tri Le <trile@users.noreply.github.com>
2019-04-25 17:54:27 +0700

    Add typed.js

--- docs/js/maket.js
+++ docs/js/maket.js
@@ -3,3 +3,9 @@ AOS.init();
 $(".nav__hamburger").click(function () {
   $(".nav__menu").toggleClass("expanded");
 });
+
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 75,
+  loop: true
+});
